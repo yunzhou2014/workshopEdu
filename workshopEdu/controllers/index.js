b6ad886b2bf60b1ed4e07858e43263d5e1404f57
Yun Zhou <zhyun1990@gmail.com>
2014-10-30 14:03:06 -0700

    add all the link pages

--- workshopEdu/controllers/index.js
+++ workshopEdu/controllers/index.js
@@ -8,4 +8,52 @@ module.exports = function (router) {
     router.get('/', function (req, res) {
         res.render('index', model);
     });
+
+    router.get('/news-events', function (req, res) {
+        res.render('layouts/newsEvents', model);
+    });
+
+    router.get('/contact', function (req, res) {
+        res.render('layouts/contact', model);
+    });
+
+    router.get('/all-courses', function (req, res) {
+        res.render('layouts/allCourses', model);
+    });
+
+    router.get('/course-detail', function (req, res) {
+        res.render('layouts/administration', model);
+    });
+
+    router.get('/staff', function (req, res) {
+        res.render('layouts/staff', model);
+    });
+
+    router.get('/about-us', function (req, res) {
+        res.render('layouts/aboutUs', model);
+    });
+
+    router.get('/blog', function (req, res) {
+        res.render('layouts/blog', model);
+    });
+
+    router.get('/blog_post', function (req, res) {
+        res.render('layouts/blogPost', model);
+    });
+
+    router.get('/typography', function (req, res) {
+        res.render('layouts/typography', model);
+    });
+
+    router.get('/javascripts', function (req, res) {
+        res.render('layouts/javascripts', model);
+    });
+    
+    router.get('/gallery', function (req, res) {
+        res.render('layouts/gallery', model);
+    });
+
+    router.get('/events-calendar', function (req, res) {
+        res.render('layouts/eventsCalendar', model);
+    });
 };
--- workshopEdu/public/js/app.js
+++ workshopEdu/public/js/app.js
@@ -11,6 +11,16 @@ require([/* Dependencies */], function () {
     var app = {
         initialize: function () {
             // Your code here
+            $(".admin").click(function(){
+            window.location='course-detail'
+            });
+            $(".contact").click(function(){
+            window.location.href='contact'
+            });
+            $(".staff").click(function(){
+            window.location='staff'
+            });
+            
         }
     };
 
