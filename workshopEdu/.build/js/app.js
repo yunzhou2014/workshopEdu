aa635d8e2a226524354a2c907311f8c1f97b298c
Yun Zhou <zhyun1990@gmail.com>
2014-10-30 23:22:13 -0700

    add gallery page, work on dropdown

--- workshopEdu/.build/js/app.js
+++ workshopEdu/.build/js/app.js
@@ -2,23 +2,45 @@
 
 
 requirejs.config({
-    paths: {}
+    paths: {
+        'jquery': '//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min',
+    }
 });
 
 
-require([/* Dependencies */], function () {
+require(['jquery'], function ($) {
 
     var app = {
         initialize: function () {
             // Your code here
             $(".admin").click(function(){
-            window.location='course-detail'
+            window.location='course-detail';
             });
             $(".contact").click(function(){
-            window.location.href='contact'
+            window.location.href='contact';
             });
             $(".staff").click(function(){
-            window.location='staff'
+            window.location='staff';
+            });
+
+            $('.thumbnail img').on('click',function(event){
+                event.preventDefault();
+                var src = $(this).attr('src');
+                var img = '<img src="' + src + '" class="img-responsive"/>';
+                $('#myModal').modal();
+                $('#myModal').on('shown.bs.modal', function(){
+                    $('#myModal .modal-body').html(img);
+                });
+                $('#myModal').on('hidden.bs.modal', function(){
+                    $('#myModal .modal-body').html('');
+                });
+           });
+
+            $('.dropdown-menu').on('click',function(event){
+                //alert("open!!");
+                //event.preventDefault();
+                console.log(event);
+                event.stopPropagation();
             });
             
         }
