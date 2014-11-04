45427976328d504b75decd404abcd2a58e0bba31
Yun Zhou <zhyun1990@gmail.com>
2014-11-03 16:51:55 -0800

    index page almost finished issue: gallery hover, modal open video instead of image

--- workshopEdu/.build/js/app.js
+++ workshopEdu/.build/js/app.js
@@ -12,37 +12,62 @@ require(['jquery'], function ($) {
 
     var app = {
         initialize: function () {
-            // Your code here
-            $(".admin").click(function(){
-            window.location='course-detail';
-            });
-            $(".contact").click(function(){
-            window.location.href='contact';
-            });
-            $(".staff").click(function(){
-            window.location='staff';
-            });
 
-            $('.thumbnail img').on('click',function(event){
-                event.preventDefault();
-                var src = $(this).attr('src');
-                var img = '<img src="' + src + '" class="img-responsive"/>';
-                $('#myModal').modal();
-                $('#myModal').on('shown.bs.modal', function(){
-                    $('#myModal .modal-body').html(img);
-                });
-                $('#myModal').on('hidden.bs.modal', function(){
-                    $('#myModal .modal-body').html('');
-                });
-           });
+          $(".admin").click(function(){
+          window.location='course-detail';
+          });
+          $(".contact").click(function(){
+          window.location.href='contact';
+          });
+          $(".staff").click(function(){
+          window.location='staff';
+          });
 
-            $('.dropdown-menu').on('click',function(event){
-                //alert("open!!");
-                //event.preventDefault();
-                console.log(event);
-                event.stopPropagation();
-            });
+          $('.thumbnail img').on('click',function(event){
+              event.preventDefault();
+              var src = $(this).attr('src');
+              var img = '<img src="' + src + '" class="img-responsive"/>';
+              $('#myModal').modal();
+              $('#myModal').on('shown.bs.modal', function(){
+                  $('#myModal .modal-body').html(img);
+              });
+              $('#myModal').on('hidden.bs.modal', function(){
+                  $('#myModal .modal-body').html('');
+              });
+          });
+
+          $('.fancybox-media').on('click',function(event){
+              event.preventDefault();
+              var src = $(this).attr("href");
+              var video = '<iframe width="100%" height="500" src="' +src + '" frameborder="0" allowfullscreen></iframe>';
+              $('#videoModal').modal();
+              $('#videoModal').on('shown.bs.modal', function(){
+                  $('#videoModal .modal-body').html(video);
+              });
+              $('#videoModal').on('hidden.bs.modal', function(){
+                  $('#videoModal .modal-body').html('');
+              });
+          });
+
+          $('#myModal button').on('click', function(){
+                $('#myModal').hide();
+          });
+
+          $('#videoModal button').on('click', function(){
+                $('#videoModal').hide();
+          });
+
+          $('.dropdown-menu').on('click',function(event){
+              //alert("open!!");
+              //event.preventDefault();
+              //console.log(event);
+              event.stopPropagation();
+          });
+
+          /*$('#calendar').fullCalendar({
             
+          });*/
+          
         }
     };
 
